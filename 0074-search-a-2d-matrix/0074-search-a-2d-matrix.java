2bc73bfbaf94ab8e9b425cca929260c3fa06a433
Anshul Pawar <anshulpawar100@gmail.com>
2023-11-05 12:40:42 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (25.83%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,62 +1,88 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = matrix.length;
-        int col = matrix[0].length;
+        int n = matrix.length;
+        int m = matrix[0].length;
 
-        int rStart = 0;
-        int rEnd = row-1;
-        int cMid = col/2;
+        int low=0,high=m*n-1;
+        //assume this 2d array as 1d array
 
-         if(row==1){
-            return binarySearch(matrix, target, 0, 0, col-1);
-        }
-        while(rStart < rEnd-1){
-            int rMid = rStart + (rEnd-rStart)/2;
-            if(matrix[rMid][cMid] == target){
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            int row=mid/m;
+            int col=mid%m;
+
+            if(matrix[row][col]==target){
                 return true;
-            }
-            else if(matrix[rMid][cMid] < target){
-                rStart = rMid;
-            }
-            else {
-                 rEnd = rMid;
+            }else if(matrix[row][col]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
             }
         }
-        if(matrix[rStart][cMid]==target){
-            return true;
-        }
-        if(matrix[rStart+1][cMid]==target){
-            return true;
-        }
-        if(cMid>0 &&  matrix[rStart][cMid-1]>=target){
-            return binarySearch(matrix,target,rStart,0,cMid-1);
-        }
-        else if(cMid<col-1 && matrix[rStart][cMid+1]<=target && matrix[rStart][col-1]>=target){
-            return binarySearch(matrix,target,rStart,cMid+1,col-1);
-        }
-        else if(cMid>0 && matrix[rStart+1][cMid-1]>=target){
-            return binarySearch(matrix,target,rStart+1,0,cMid-1);
-        }
-        else {
-            return binarySearch(matrix,target,rStart+1,cMid+1,col-1);
-        }
-    }
-    public static boolean binarySearch(int [][] matrix,int target,int r,int c1,int c2)  {           
-        int start = c1 ;
-        int end = c2;
+        return false;
 
-            while(start<=end){
-                int cMid = start + (end-start)/2;
-                if(matrix[r][cMid]==target){
-                    return true;
-                }
-                else if(matrix[r][cMid]<target){
-                    start=cMid+1;
-                }
-                else{
-                    end = cMid-1;
-                }
-            }
-            return false;
     }
 }
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int row = matrix.length;
+//         int col = matrix[0].length;
+
+//         int rStart = 0;
+//         int rEnd = row-1;
+//         int cMid = col/2;
+
+//          if(row==1){
+//             return binarySearch(matrix, target, 0, 0, col-1);
+//         }
+//         while(rStart < rEnd-1){
+//             int rMid = rStart + (rEnd-rStart)/2;
+//             if(matrix[rMid][cMid] == target){
+//                 return true;
+//             }
+//             else if(matrix[rMid][cMid] < target){
+//                 rStart = rMid;
+//             }
+//             else {
+//                  rEnd = rMid;
+//             }
+//         }
+//         if(matrix[rStart][cMid]==target){
+//             return true;
+//         }
+//         if(matrix[rStart+1][cMid]==target){
+//             return true;
+//         }
+//         if(cMid>0 &&  matrix[rStart][cMid-1]>=target){
+//             return binarySearch(matrix,target,rStart,0,cMid-1);
+//         }
+//         else if(cMid<col-1 && matrix[rStart][cMid+1]<=target && matrix[rStart][col-1]>=target){
+//             return binarySearch(matrix,target,rStart,cMid+1,col-1);
+//         }
+//         else if(cMid>0 && matrix[rStart+1][cMid-1]>=target){
+//             return binarySearch(matrix,target,rStart+1,0,cMid-1);
+//         }
+//         else {
+//             return binarySearch(matrix,target,rStart+1,cMid+1,col-1);
+//         }
+//     }
+//     public static boolean binarySearch(int [][] matrix,int target,int r,int c1,int c2)  {           
+//         int start = c1 ;
+//         int end = c2;
+
+//             while(start<=end){
+//                 int cMid = start + (end-start)/2;
+//                 if(matrix[r][cMid]==target){
+//                     return true;
+//                 }
+//                 else if(matrix[r][cMid]<target){
+//                     start=cMid+1;
+//                 }
+//                 else{
+//                     end = cMid-1;
+//                 }
+//             }
+//             return false;
+//     }
+// }
