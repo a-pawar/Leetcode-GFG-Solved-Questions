e172a0b9ad3bb579e6d6ba26d4706fdbccc914a9
Anshul Pawar <anshulpawar100@gmail.com>
2023-11-09 10:35:30 +0530

    Time: 14 ms (16.24%), Space: 44.1 MB (31.77%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,29 +1,44 @@
 class Solution {
     public String reverseWords(String s) {
-        int left=0,right=s.length()-1;
+//         int left=0,right=s.length()-1;
 
-				String temp="",ans="";
-				while(left<=right){
-					if(s.charAt(left)!=' '){
-						temp+=s.charAt(left);
-					}else if(!temp.equals("")){
-						if(ans.equals("")){
-							ans = temp;
-						}else{
-							ans=temp+" "+ans;
-						}
-						temp="";
-					}
-					left++;
-				}
-				//add last word
-				if(!temp.equals("")){
-					if(ans.equals("")){
-							ans = temp;
-					}else{
-						ans=temp+" "+ans;
-					}
-				}
-				return ans;
+// 				String temp="",ans="";
+// 				while(left<=right){
+// 					if(s.charAt(left)!=' '){
+// 						temp+=s.charAt(left);
+// 					}else if(!temp.equals("")){
+// 						if(ans.equals("")){
+// 							ans = temp;
+// 						}else{
+// 							ans=temp+" "+ans;
+// 						}
+// 						temp="";
+// 					}
+// 					left++;
+// 				}
+// 				//add last word
+// 				if(!temp.equals("")){
+// 					if(ans.equals("")){
+// 							ans = temp;
+// 					}else{
+// 						ans=temp+" "+ans;
+// 					}
+// 				}
+// 				return ans;
+        
+        // Trim the input string to remove leading and trailing spaces
+        String[] str = s.trim().split("\\s+");
+
+        // Initialize the output string
+        String out = "";
+
+        // Iterate through the words in reverse order
+        for (int i = str.length - 1; i > 0; i--) {
+            // Append the current word and a space to the output
+            out += str[i] + " ";
+        }
+
+        // Append the first word to the output (without trailing space)
+        return out + str[0];
     }
 }
