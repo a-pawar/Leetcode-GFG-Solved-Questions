f3839a044541fbeeccdfa94860a2a6037466ced1
Anshul Pawar <anshulpawar100@gmail.com>
2023-11-14 10:50:43 +0530

    Time: 6 ms (92.62%), Space: 41.5 MB (31.56%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,21 +1,35 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character,Character> map = new HashMap<>();
-        Set<Character> set = new HashSet<>();
-        //traverse s string
+    // approach 1:
+        // Map<Character,Character> map = new HashMap<>();
+        // Set<Character> set = new HashSet<>();
+        // //traverse s string
+        // for(int i=0;i<s.length();i++){
+        //     //check map contain character at i in s string , if it contains it means we have mapped that character previously,so check with mapping
+        //     if(map.containsKey(s.charAt(i)) && map.get(s.charAt(i))!=t.charAt(i)){
+        //             return false;
+        //     }
+        //     //this case is for s="badc" t="baba" b alresdy map to b and d also try to map with b it is not allow
+        //     if(!map.containsKey(s.charAt(i)) && set.contains(t.charAt(i))){
+        //         return false;
+        //     }
+        //     //map character and add in set
+        //     map.put(s.charAt(i),t.charAt(i));
+        //     set.add(t.charAt(i));
+        // }
+        // return true;
+    // optimise
+        
+        int map1[]=new int[256];
+        int map2[]=new int[256];
         for(int i=0;i<s.length();i++){
-            //check map contain character at i in s string , if it contains it means we have mapped that character previously,so check with mapping
-            if(map.containsKey(s.charAt(i)) && map.get(s.charAt(i))!=t.charAt(i)){
-                    return false;
-            }
-            //this case is for s="badc" t="baba" b alresdy map to b and d also try to map with b it is not allow
-            if(!map.containsKey(s.charAt(i)) && set.contains(t.charAt(i))){
+            if(map1[s.charAt(i)]!=map2[t.charAt(i)]){
                 return false;
             }
-            //map character and add in set
-            map.put(s.charAt(i),t.charAt(i));
-            set.add(t.charAt(i));
+            map1[s.charAt(i)]=i+1;
+            map2[t.charAt(i)]=i+1;
         }
         return true;
+        
     }
 }
