5673e15d60a02afa25b174758e4fbe5253281c10
Anshul Pawar <anshulpawar100@gmail.com>
2023-11-17 09:16:56 +0530

    Time: 71 ms (44.80%), Space: 42.3 MB (29.03%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,44 +1,25 @@
 class Solution {
     public int beautySum(String s) {
-        int sum=0;
-        Map<Character,Integer> map;
+        int least=Integer.MAX_VALUE,most=Integer.MIN_VALUE;
+        int sum=0, beuty=0;
         for(int i=0;i<s.length();i++){
-            map=new HashMap<>();
+            int [] freq=new int[26];
             for(int j=i;j<s.length();j++){
                 //for every substring
-                int [] arr=calculateDifference(s,i,j,map);
-                //arr[0] ---least  arr[1]----most
-                
-                sum=sum+(arr[1]-arr[0]);
+                freq[s.charAt(j)-'a']++;
+                for(int k=0;k<26;k++){
+                    if(freq[k]<least && freq[k]!=0){
+                        least=freq[k];
+                    }
+                    if(freq[k]>most && freq[k]!=0){
+                        most=freq[k];
+                    }
+                }
+                beuty=most-least;
+                sum = sum+beuty;
+                least=Integer.MAX_VALUE;most=Integer.MIN_VALUE;
             }
         }
         return sum;
-    }
-    public int[] calculateDifference(String s,int i,int j,Map<Character,Integer> map){
-        
-        int least=Integer.MAX_VALUE,most=Integer.MIN_VALUE;
-        //traverse substring //map each character
-        // while(i<=j){
-            char ch=s.charAt(j);
-            if(map.containsKey(ch)){
-                int val = map.get(ch);
-                map.put(ch,val+1);
-            }else{
-                map.put(ch,1);
-            }
-            // i++;
-        // }
-        Set<Character> keys =  map.keySet();
-        for(char key:keys){
-            int val = map.get(key);
-            if(val<least){
-                least=val;
-            }
-            if(val>most){
-                most=val;
-            }
-            
-        }
-        return new int[]{least,most};
     }
 }
