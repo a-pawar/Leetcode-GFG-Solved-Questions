bf4b6a95ca5a053e57e7bbd6c71f8cc5a61285ac
Anshul Pawar <anshulpawar100@gmail.com>
2023-11-17 11:42:11 +0530

    Time: 4 ms (80.79%), Space: 55.1 MB (96.25%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,13 +11,54 @@
 import java.util.*;
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ArrayList<Integer> arr=new ArrayList<>();
-        while(head != null) {
-            arr.add(head.val);
-            head = head.next;
+        // //bruteforce
+        // ArrayList<Integer> arr=new ArrayList<>();
+        // while(head != null) {
+        //     arr.add(head.val);
+        //     head = head.next;
+        // }
+        // for(int i=0;i<arr.size()/2;i++) 
+        //     if(arr.get(i) != arr.get(arr.size()-i-1)) return false;
+        // return true;
+        
+        //optimized
+        //if list has no elment or only one element
+        if(head==null || head.next==null){
+            return true;
         }
-        for(int i=0;i<arr.size()/2;i++) 
-            if(arr.get(i) != arr.get(arr.size()-i-1)) return false;
+        //first find the midle of LL
+        ListNode slow=head,fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        //now reverse the list from middle
+        ListNode revHead= reverseLL(slow.next);
+        slow.next=revHead;
+        slow=slow.next;
+        //now compare from start and middle
+        ListNode temp = head;
+        while(slow!=null){
+            if(temp.val != slow.val){
+                return false;
+            }
+            temp=temp.next;
+            slow=slow.next;
+        }
         return true;
+
+    }
+    public ListNode reverseLL(ListNode head ){
+        ListNode prev = null;
+        
+        ListNode store = head.next;
+        while(store!=null){
+            head.next=prev;
+            prev=head;
+            head=store;
+            store=store.next;
+        }
+        head.next=prev;
+        return head;
     }
 }
