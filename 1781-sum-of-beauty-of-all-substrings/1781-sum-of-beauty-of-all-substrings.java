60f34ca95c19dd5ac8f0c542852849c2857b87cb
Anshul Pawar <anshulpawar100@gmail.com>
2023-11-17 09:22:33 +0530

    Time: 72 ms (44.58%), Space: 41.8 MB (51.70%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -2,11 +2,9 @@ class Solution {
     public int beautySum(String s) {
         int least=Integer.MAX_VALUE,most=Integer.MIN_VALUE;
         int sum=0, beuty=0;
-        int [] freq=new int[26];
+        int [] freq;
         for(int i=0;i<s.length();i++){
-             for(int k=0;k<26;k++){
-                 freq[k]=0;
-             }
+             freq=new int[26];
             for(int j=i;j<s.length();j++){
                 //for every substring
                 freq[s.charAt(j)-'a']++;
