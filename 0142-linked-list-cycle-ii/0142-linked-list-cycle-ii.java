2820bd67a31cb101acb64c25d6d7dbf232064a63
Anshul Pawar <anshulpawar100@gmail.com>
2023-11-17 11:10:37 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (26.89%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,16 +11,46 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
-        ListNode temp = head;
-        while(temp!=null){
-            if(set.contains(temp)){
-                return temp;
-            }
-            set.add(temp);
-            temp=temp.next;
+        // //save the in set and check if it found means there is cycle
+        // Set<ListNode> set = new HashSet<>();
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     if(set.contains(temp)){
+        //         return temp;
+        //     }
+        //     set.add(temp);
+        //     temp=temp.next;
+        // }
+        // return null;
+
+        //slow fast pointer approach
+        if(head==null || head.next==null){
+            return null;
         }
-        return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean cycle=false;
+
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                cycle=true;
+                break;
+            }
+        }
+        //when slow and fast equal,remaining distance for 1st cycle node is eqaul to distance from head
+        if(cycle){
+            ListNode temp = head;
+            while(slow!=temp){
+                slow=slow.next;
+                temp=temp.next;
+            }
+            return slow;
+        }
+        else{
+            return null;
+        }
 
     }
 }
