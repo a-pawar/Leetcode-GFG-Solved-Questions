7a245a2b1375180c561ad1539c05af53b9049649
Anshul Pawar <anshulpawar100@gmail.com>
2023-12-21 22:05:30 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (5.82%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int nums[] = new int[n];
+        for(int i=0;i<nums.length;i++){
+            nums[i]=start+2*i;
+        }
+        int ans=0;
+        
+        for(int i=0;i<nums.length;i++){
+            ans=nums[i]^ans;
+        }
+        return ans;
+
+
+    }
+}
