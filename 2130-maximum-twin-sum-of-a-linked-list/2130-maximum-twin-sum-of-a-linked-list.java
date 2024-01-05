d0351c1f9a6d21fb60a8cd2e8c2dff6117d06423
Anshul Pawar <anshulpawar100@gmail.com>
2024-01-05 11:58:18 +0530

    Time: 5 ms (70.00%), Space: 63.5 MB (64.70%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -10,8 +10,8 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        
-        int n=0;
+    //optimize 
+        int n=0;//size
         ListNode node =head;
         while(node!=null){
             n++;
@@ -22,23 +22,59 @@ class Solution {
             slow=slow.next;
             fast=fast.next.next;
         }
-        //now slow at the mid of list so put last half list in stack
-        Stack<Integer> stack=new Stack<>();
-        while(slow!=null){
-            stack.push(slow.val);
-            slow=slow.next;
+        ListNode prev=null,curr=slow,nextnode=slow.next;
+        while(curr!=null){
+            curr.next=prev;
+            prev=curr;
+            curr=nextnode;
+            if(nextnode!=null){
+                nextnode=nextnode.next;
+            }
         }
-        node =head;
+        ListNode reversehead=prev;
+        node=head;
         int maxsum=0;
         for(int i=0;i<=(n/2)-1;i++){
-            int lastelement = stack.pop();
-           int sum  = node.val + lastelement;
+        
+           int sum  = node.val + reversehead.val;
             if(maxsum<sum){
                 maxsum=sum;
             }
             node=node.next;
+            reversehead=reversehead.next;
         }
         return maxsum;
+        
+        
+    //Moderate
+//         int n=0;
+//         ListNode node =head;
+//         while(node!=null){
+//             n++;
+//             node=node.next;
+//         }
+//         ListNode slow=head,fast=head;
+//         while(fast!=null && fast.next!=null){
+//             slow=slow.next;
+//             fast=fast.next.next;
+//         }
+//         //now slow at the mid of list so put last half list in stack
+//         Stack<Integer> stack=new Stack<>();
+//         while(slow!=null){
+//             stack.push(slow.val);
+//             slow=slow.next;
+//         }
+//         node =head;
+//         int maxsum=0;
+//         for(int i=0;i<=(n/2)-1;i++){
+//             int lastelement = stack.pop();
+//            int sum  = node.val + lastelement;
+//             if(maxsum<sum){
+//                 maxsum=sum;
+//             }
+//             node=node.next;
+//         }
+//         return maxsum;
         
     }
 }
