0130175c859f07eb387ae7b6da0a44155c18bfdb
Anshul Pawar <anshulpawar100@gmail.com>
2024-01-11 15:24:01 +0530

    Time: 16 ms (46.52%), Space: 54.1 MB (16.42%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,67 +1,45 @@
- class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
 class MyHashSet {
-    ListNode head;
-    ListNode tail;
-
+    LinkedList<Integer> [] arr;
+    int m = 5000;//size of bucket
+    public int hashing(int key){
+        int index = key%m;
+        return index;
+    }
     public MyHashSet() {
+        arr = new LinkedList[5000];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=new LinkedList<>();
+        }
+    }
+
+    
+    public void add(int key) {
+        int index = hashing(key);
+        LinkedList<Integer> list = arr[index];
+        boolean iscontain = contains(key);
+        if(!iscontain){
+            list.add(key);
+        }
         
     }
     
-    public void add(int key) {
-        ListNode newnode = new ListNode(key);
-        if(!contains(key)){
-            if(head==null){
-                head=newnode;
-                tail=newnode;
-            }else{
-                tail.next=newnode;
-                tail=newnode;
-            }
-        }
-    }
-    
     public void remove(int key) {
-        ListNode node =head,prev=null;
-        while(node!=null){
-            if(node.val==key){
-                if(head==node){
-                    head=head.next;
-                    //only one node in list
-                    if(head==tail){
-                        tail=head;
-                    }
-                }else{
-                    prev.next=node.next;
-                    if(tail==node){
-                        tail=prev;
-                    }
-                }
-                break;
-            }
-            prev=node;
-            node=node.next;   
+        int index = hashing(key);
+        LinkedList<Integer> list = arr[index];
+        int keyindex = list.indexOf(key);
+        if(keyindex!=-1){
+            list.remove(keyindex);
         }
         
     }
     
     public boolean contains(int key) {
-        ListNode node = head;
-        
-        while(node!=null){
-            if(node.val==key){
-                
-                return true;
-            }
-            node=node.next;
+        int index = hashing(key);
+        LinkedList<Integer> list = arr[index];
+        if(list.size()==0){
+            return false;
         }
-        return false;
-        
+        return list.contains(key);
     }
 }
 
