27caa8f7226152354810d388509312f6c630f04f
Anshul Pawar <anshulpawar100@gmail.com>
2024-03-05 11:48:49 +0530

    Time: 2 ms (92.78%), Space: 63.8 MB (9.17%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -18,19 +18,25 @@ class Solution {
         if(root==null){
             return 0;
         }
-        if(root.left==null && root.right==null){
-            return 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level=0;
+        while(!queue.isEmpty()){
+            int len = queue.size();
+            for(int i=0;i<len;i++){
+                TreeNode node = queue.poll();
+                if(node.left==null && node.right==null){
+                    return level+1;
+                }
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                } 
+            }
+            level++;
         }
-        int leftans=Integer.MAX_VALUE,rightans = Integer.MAX_VALUE;
-        if(root.left!=null){
-             leftans = minDepth(root.left);
-        }
-        if(root.right!=null){
-             rightans = minDepth(root.right);
-        }
-
-        int ans = 1+Math.min(leftans,rightans);
-        return ans;
-
+        return 0;
     }
 }
