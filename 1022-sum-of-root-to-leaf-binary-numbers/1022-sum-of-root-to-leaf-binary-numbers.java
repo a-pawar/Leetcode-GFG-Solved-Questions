287a8ac5aa3ced3ef9ad712634c7faf56ad07b16
Anshul Pawar <anshulpawar100@gmail.com>
2024-03-10 18:27:49 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (55.91%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -15,30 +15,23 @@
  */
 class Solution {
     public int sumRootToLeaf(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        return helper(root,list);
+        
+        return helper(root,0);
     }
-    public int helper(TreeNode node , List<Integer> list){
+    public int helper(TreeNode node ,int sum){
+        if(node==null){
+            return 0;
+        }
+        sum=2*sum+node.val;
+
         if(node.left==null && node.right==null){
-            list.add(node.val);
-            int temp=0;
-            int pow=list.size()-1;
-            for(int i=0;i<list.size();i++){
-                temp=temp+ list.get(i)* (int)(Math.pow(2,pow));
-                pow--;
-            }
-            list.remove(list.size()-1);
-            return temp;
+            return sum;
         }
         int leftans=0,rightans=0;
-        list.add(node.val);
-        if(node.left!=null){
-            leftans=helper(node.left,list);
-        }
-        if(node.right!=null){
-           rightans= helper(node.right,list);
-        }
-        list.remove(list.size()-1);
+            leftans=helper(node.left,sum);
+        
+           rightans= helper(node.right,sum);
+        
         return leftans+rightans;
     }
 }
