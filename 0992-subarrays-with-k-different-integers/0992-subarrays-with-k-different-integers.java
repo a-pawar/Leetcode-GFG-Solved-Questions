654da659e8910b66eeb1eaa65fd0b40811dd6aa0
Anshul Pawar <anshulpawar100@gmail.com>
2024-03-30 12:32:25 +0530

    Time: 44 ms (59.86%), Space: 47.8 MB (16.20%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return slidingWindow(nums,k)-slidingWindow(nums,k-1);
+    }
+    //total count of subarray <=k
+    public int slidingWindow(int [] nums,int k){
+        int n = nums.length;
+        HashMap<Integer,Integer> map =new HashMap<>();
+        int i=0,j=0;
+        
+        int count=0;
+        
+        while(j<n){
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+            
+            while(map.size()>k){
+                map.put(nums[i],map.get(nums[i])-1);
+                if(map.get(nums[i])==0){
+                    map.remove(nums[i]);
+                }
+                i++;
+            }
+            //all subarray ending at j
+            count += (j-i+1);
+            
+            j++;
+        }
+        return count;
+    }
+}
