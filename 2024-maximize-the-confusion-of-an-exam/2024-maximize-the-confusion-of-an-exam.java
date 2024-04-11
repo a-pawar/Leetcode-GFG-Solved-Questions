741f6c7459cdb330e7f9e5be2fc55f5692c58b0d
Anshul Pawar <anshulpawar100@gmail.com>
2024-04-11 18:29:45 +0530

    Time: 11 ms (91.51%), Space: 44.5 MB (79.50%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,42 +1,70 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
+        //one pass sliding window
         int n=answerKey.length();
-        //when F convert to T
-        int count1=0;
-        int l=0,r=0,temp=k;
-        while(r<n){
-            if(answerKey.charAt(r)=='F'){
-                temp--;
-            }
-            while(temp<0 && l<=r){
-                if(answerKey.charAt(l)=='F'){
-                    temp++;
-                }
-                l++;
-            } 
-                
-            
-            count1 =Math.max(count1,r-l+1);
-            r++;
-        }
-        //when T convert to F
-        l=0;r=0;temp=k;
-        int count2=0;
+        int countF=0,countT=0;
+        int l=0,r=0,ans=0;
         while(r<n){
             if(answerKey.charAt(r)=='T'){
-                temp--;
+                countT++;
+            }else{
+                countF++;
             }
-            while(temp<0 && l<=r){
+            //flip minimum number
+            int min = Math.min(countT,countF);
+            //if min is more than k we cannot flip min element so reduce the window
+            while(min>k && l<=r){
                 if(answerKey.charAt(l)=='T'){
-                    temp++;
+                    countT--;
+                }else{
+                    countF--;
                 }
+                 min = Math.min(countT,countF); 
                 l++;
-            } 
+            }
+            ans = Math.max(ans,r-l+1);
+            r++;
+            
+        }
+        return ans;
+//         //two pass sliding window
+//         int n=answerKey.length();
+//         //when F convert to T
+//         int count1=0;
+//         int l=0,r=0,temp=k;
+//         while(r<n){
+//             if(answerKey.charAt(r)=='F'){
+//                 temp--;
+//             }
+//             while(temp<0 && l<=r){
+//                 if(answerKey.charAt(l)=='F'){
+//                     temp++;
+//                 }
+//                 l++;
+//             } 
                 
             
-            count1 =Math.max(count1,r-l+1);
-            r++;
-        }
-        return count1<count2?count2:count1;
+//             count1 =Math.max(count1,r-l+1);
+//             r++;
+//         }
+//         //when T convert to F
+//         l=0;r=0;temp=k;
+//         int count2=0;
+//         while(r<n){
+//             if(answerKey.charAt(r)=='T'){
+//                 temp--;
+//             }
+//             while(temp<0 && l<=r){
+//                 if(answerKey.charAt(l)=='T'){
+//                     temp++;
+//                 }
+//                 l++;
+//             } 
+                
+            
+//             count1 =Math.max(count1,r-l+1);
+//             r++;
+//         }
+//         return count1<count2?count2:count1;
     }
 }
